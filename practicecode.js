fabb2e47ea23ce8286b972e255b6a065e6315d0c
Jessica Standage <Jessica@Jessicas-MBP.socal.rr.com>
2019-05-04 12:05:13 -0700

    Number 2
    Trying to write a function that will reverse the contents of an array.

--- practicecode.js
+++ practicecode.js
@@ -1,1 +1,10 @@
-Some code will go here when I write it
+Exercise 2:
+
+function reverseArrayInPlace(arr) {
+  for (var i = 0; i <= Math.floor((arr.length - 1) / 2); i++) {
+    let el = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = el;
+  }
+  return arr;
+}
